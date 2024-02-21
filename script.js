2fea1903aee6f157774705179c7389a45f60bacc
Khushpreet <khushpreetsingh2047@gmail.com>
2024-02-21 15:49:46 +0530

    made carousal

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-userList = [
-  {
-    image: "customer-1.png",
-    name: "Viezh Robert",
-    location: "Warsaw, Poland",
-    ratings: "4.5",
-    review:
-      "“Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.",
-  },
-  {
-    image: "customer-2.png",
-    name: "Viezh Robert",
-    location: "Warsaw, Poland",
-    ratings: "4.5",
-    review:
-      "“Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.",
-  },
-  {
-    image: "customer-3.png",
-    name: "Viezh Robert",
-    location: "Warsaw, Poland",
-    ratings: "4.5",
-    review:
-      "“Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.",
-  },
-];
+// #pricing
+let premiumplan = document.getElementById('premium')
+  premiumplan.style.border = "2px solid red"
 
+const select = (id)=>{
+  let freeplan = document.getElementById('free')
+  let standardplan = document.getElementById('standard')
+  let premiumplan = document.getElementById('premium')
+
+  if(id == 1){
+    freeplan.style.border = '2px solid #f53838'
+    standardplan.style.border ='2px solid #dddddd'
+    premiumplan.style.border ='2px solid #dddddd'
+
+  }else if(id==2){
+    freeplan.style.border ='2px solid #dddddd'
+    standardplan.style.border = '2px solid #f53838'
+    premiumplan.style.border ='2px solid #dddddd'
+  }else if(id==3){
+    freeplan.style.border ='2px solid #dddddd'
+    standardplan.style.border ='2px solid #dddddd'
+    premiumplan.style.border = '2px solid #f53838'
+  }
+}
+
+
+// #testimonial
 let counter = 0;
 let customer1 = document.getElementById("customer1");
 let customer2 = document.getElementById("customer2");
@@ -38,64 +38,76 @@ customer3.style.border = '2px solid #f53838'
 
 const next = () => {
   if (counter == 0) {
-    customer1.style.transform = `translateX(-112%)`;
-    customer2.style.transform = `translateX(-112%)`;
-    customer3.style.transform = `translateX(226%)`;
-
-    customer2.style.border = '2px solid #f53838'
-    customer3.style.border ='2px solid #76787a'
-
+    change(0)
     counter++
   } else if(counter == 1){
-    customer1.style.transform = `translateX(-225%)`;
-    customer2.style.transform = `translateX(+113%)`;
-    customer3.style.transform = `translateX(113%)`;
-
-    customer1.style.border = '2px solid #f53838'
-    customer2.style.border ='2px solid #76787a'
-
-
+    change(1)
     counter++
   }else if(counter == 2){
-    customer1.style.transform = `translateX(0%)`;
-    customer2.style.transform = `translateX(0%)`;
-    customer3.style.transform = `translateX(0%)`;
-
-    
-    customer3.style.border = '2px solid #f53838'
-    customer1.style.border ='2px solid #76787a'
-
+    change(2)
     counter = 0
   }
 }
 
 const previous = () => {
   if (counter == 0) {
-    customer1.style.transform = `translateX(-225%)`;
-    customer2.style.transform = `translateX(+113%)`;
-    customer3.style.transform = `translateX(113%)`;
-
-    customer1.style.border = '2px solid #f53838'
-    customer3.style.border ='2px solid #76787a'
-
+    change(0)
     counter++
   } else if(counter == 1){
-    customer1.style.transform = `translateX(-112%)`;
-    customer2.style.transform = `translateX(-112%)`;
-    customer3.style.transform = `translateX(226%)`;
-
-    customer2.style.border = '2px solid #f53838'
-    customer1.style.border ='2px solid #76787a'
-
+    change(1)
     counter++
   }else if(counter == 2){
+    change(2)
+    counter = 0
+  }
+};
+
+let selector1 = document.getElementById('selector1')
+selector1.style.backgroundColor='#f53838'
+
+const change = (id)=>{
+  let selector1 = document.getElementById('selector1')
+  let selector2 = document.getElementById('selector2')
+  let selector3 = document.getElementById('selector3')
+  if(id==0){
     customer1.style.transform = `translateX(0%)`;
     customer2.style.transform = `translateX(0%)`;
     customer3.style.transform = `translateX(0%)`;
 
+    selector1.style.backgroundColor='#f53838'
+    selector2.style.backgroundColor='#dee0e0'
+    selector3.style.backgroundColor='#dee0e0'
+    
     customer3.style.border = '2px solid #f53838'
     customer2.style.border ='2px solid #76787a'
+    customer1.style.border ='2px solid #76787a'
 
-    counter = 0
+  }else if(id==1){
+    customer1.style.transform = `translateX(-112%)`;
+    customer2.style.transform = `translateX(-112%)`;
+    customer3.style.transform = `translateX(226%)`;
+
+    selector2.style.backgroundColor='#f53838'
+    selector1.style.backgroundColor='#dee0e0'
+    selector3.style.backgroundColor='#dee0e0'
+
+    customer2.style.border = '2px solid #f53838'
+    customer3.style.border ='2px solid #76787a'
+    customer1.style.border ='2px solid #76787a'
+  }else if(id ==2){
+
+    customer1.style.transform = `translateX(-225%)`;
+    customer2.style.transform = `translateX(+113%)`;
+    customer3.style.transform = `translateX(113%)`;
+
+    selector3.style.backgroundColor='#f53838'
+    selector1.style.backgroundColor='#dee0e0'
+    selector2.style.backgroundColor='#dee0e0'
+
+    customer1.style.border = '2px solid #f53838'
+    customer2.style.border ='2px solid #76787a'
+    customer3.style.border ='2px solid #76787a'
+
   }
-};
+}
+9828645700
